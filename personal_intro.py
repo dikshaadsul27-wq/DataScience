87ad0268dd6e9a07e74326dce878b21e31330cb2
dikshaadsul27-wq <dikshaadsul27@gmail.com>
2026-03-16 20:59:23 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,9 @@
+# personal_intro.py
+
+#Collect user information
+name = input("What is your name? :")
+age = input("What old are you? :")
+hobby = input("What is favorite hobby? :")
+
+#Display a personalised welcome message
+print(f"Hello {name}, you are {age} years old and love {hobby}")
